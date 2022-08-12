e4f6fa39b2e2ee2b6c44a3839941a78b6996bfa6
EmilyShannon <110046867+EmilyShannon@users.noreply.github.com>
2022-08-11 22:36:02 -0300

    Add files via upload

--- tumblrScraper.py
+++ tumblrScraper.py
@@ -3,12 +3,17 @@ import os
 import time
 import pytumblr
 import numpy as np
+import json as js
+
+#Tracks how many requests have been sent to the server
+count = 0
 
 #Collect posts under a given tag and print their urls
 def get_all_notes_and_tags(client, tag:str, all_unique_tags):
+    global count
     
-    #make a loop to be able toget more posts
-    for i in range(4):
+    #make a loop to be able to get more posts
+    for i in range(24):
 
         min_timestamp=0
 
@@ -17,23 +22,27 @@ def get_all_notes_and_tags(client, tag:str, all_unique_tags):
         #Initialize the list of posts using the dedault (current) timestamp
         if(min_timestamp==0):
             response = client.tagged(tag, limit=20)
+            #This is a request so increment the count
+            count+=1
+        
         #Otherwise, use the min timestamp
         else:
+            count+=1
             response = client.tagged(tag, before=min_timestamp, limit=20)
 
-        #Catches status 429 error
-        if response['meta']['status'] == 429:
+       
+        #Particular error if the server refusing to take more requests
+        if 'meta' in response:
             yield 'too many requests'
 
         #Track the timestamps to find the oldest one and then search for more posts before that
         min_timestamp=response[0]['timestamp']
-        
-        #time.sleep(60)
+
         #For every post, get the tags and notes
         for post in response:
             timestamp = post['timestamp']
             
-            #Update the earliest time
+            #Update the earliest timeS
             if(timestamp<min_timestamp):
                 min_timestamp=timestamp
             
@@ -45,11 +54,18 @@ def get_all_notes_and_tags(client, tag:str, all_unique_tags):
                 for tag in tags:
                     if(tag not in all_unique_tags):
                         all_unique_tags.append(tag)
-                #time.sleep(2)
+     
+
+                #Get the notes on that post and update the number of requests sent
                 notes = client.notes(post['blog_name'], post['id'])
-                yield ['tags:' + str(tags), 'notes: ' + str(notes)]
+                count+=1
+
+                yield {"tags": str(tags), "notes": str(notes)}
+        
             else:
+                #If for some reason te post itself can't be retrieved
                 print("Can't find ID for post")
+        
 
 #Define an exception that will be used when too many requests have been sent to the server 
 class Error(Exception):
@@ -64,7 +80,7 @@ client = pytumblr.TumblrRestClient('9qrx1tFz5fkXYRbyoxK2r4D5JKZNzVqkVxg7DEa7Wm4v
 #Make a list of all unique tags to keep feeding the function
 #There's one in there as a seed 
 all_unique_tags = ['thinspo']
-os.chdir(f"{os.getcwd()}\\tumblrPostInfo")
+os.chdir("C:\\Users\\User\\Downloads\\tumblrPostInfo")
 i=0
 
 #The list of tags will change 
@@ -72,18 +88,46 @@ while i<len(all_unique_tags):
     
     #The list will hopefully expand as we go
     tag=all_unique_tags[i]
-    print(i)
-    try: 
-        with open(f'{tag}-posts.txt', 'w', encoding='utf-8') as file:
-            for notes in get_all_notes_and_tags(client, tag, all_unique_tags):
-                if(notes == 'too many requests'):
-                    raise TooManyRequestsError
-                file.write(str(notes))
-        print(f'{tag} done')
-    except TooManyRequestsError:
-        print('Sent too many requests. Wait for a bit.')
-    except:
-        print(f'{tag} not done')
-    
 
-    i+=1
+    #Check if it's already been done
+    if tag!='thinspo' and os.path.exists(f'{os.getcwd()}\\{tag}-posts.json') and os.path.getsize('os.getcwd()}\\{tag}-posts.json') != 0:
+        i+=1
+        continue
+    else:
+        #print(i)
+        try: 
+            #Make a json file for each tag
+            with open(f'{tag}-posts.json', 'w', encoding='utf-8') as file:
+                
+                #Overall list that will be written to the file
+                posts_info=[]
+
+                #This works b/c of using yield
+                for notes in get_all_notes_and_tags(client, tag, all_unique_tags):
+                    if(notes == 'too many requests'):   
+                        raise TooManyRequestsError
+
+                    else:
+                        posts_info.append(notes)
+           
+
+                #Write the list of posts to the json file
+                js.dump(posts_info, file)
+
+    
+                #print('Num of requests sent: ' + str(count))
+            print(f'{tag} done')
+
+        #For either exception, the program terminates
+        except TooManyRequestsError:
+            #print('Num of requests sent: ' +str(count))
+            print('Sent too many requests. Wait for a bit.')
+            break
+
+            
+        except Exception as e:
+            #with Exception as e:
+            print(f'Exception occurred: {e}')
+            break
+
+        i+=1
